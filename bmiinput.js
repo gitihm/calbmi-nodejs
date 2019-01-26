34a427cdc9274e3f9e4eff5d95e2d05c9f54de2a
gitihm <ismael897@outlook.com>
2019-01-26 12:46:56 +0700

    add ifelse

--- bmiinput.js
+++ bmiinput.js
@@ -3,9 +3,9 @@ var data = {
     result : 0
 }
 exports = input = () =>{
-    height = readline.question("What hight are you? : ")
-    weight = readline.question("What weight are you? : ")
-    console.log(height + " " + weight)
+    height = readline.question("What hight are you? (cm3): ")
+    weight = readline.question("What weight are you? (kg): ")
+    //console.log(height + " " + weight)
      data.result = parseFloat(weight)/Math.pow((parseFloat(height)/100),2) 
     return JSON.stringify(data)
 }
